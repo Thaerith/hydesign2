c6c6fd92c67d87bfd985a5da85f12dad7c24e792
Benjamin Malengreau <benjamin.malengreau@outlook.com>
2026-01-16 15:17:41 +0100

    define IconButton

---
+++ src/commonMain/kotlin/com/imiplc/hydesign2/input/IconButton.kt
@@ -0,0 +1,23 @@
+package com.imiplc.hydesign2.input
+
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.material3.IconButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun IconButton(
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true,
+    interactionSource: MutableInteractionSource? = null,
+    content: @Composable () -> Unit,
+) {
+    IconButton(
+        onClick = onClick,
+        modifier = modifier,
+        enabled = enabled,
+        interactionSource = interactionSource,
+        content = content,
+    )
+}
