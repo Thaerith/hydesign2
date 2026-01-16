7c6824377f9620dce6dad93e4521138fabb7e0ac
Benjamin Malengreau <benjamin.malengreau@outlook.com>
2026-01-16 16:40:30 +0100

    define Dialog

---
+++ src/commonMain/kotlin/com/imiplc/hydesign2/layout/Dialog.kt
@@ -0,0 +1,29 @@
+package com.imiplc.hydesign2.layout
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.DialogProperties
+import com.imiplc.hydesign2.display.Title
+
+@Composable
+fun Dialog(
+    title: String,
+    onDismissRequest: () -> Unit,
+    properties: DialogProperties = DialogProperties(),
+) {
+    Dialog(
+        onDismissRequest = onDismissRequest,
+        properties = properties,
+    ) {
+        Card {
+            Column(modifier = Modifier.padding(8.dp)) {
+                Title(title)
+            }
+        }
+    }
+}
